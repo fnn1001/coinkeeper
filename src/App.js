096d0d2828f7ae80d05eb41a250376a3db5a165d
leamonsenego <lea.monsenego@gmail.com>
2023-11-12 15:01:17 +0100

    Landing page styling done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+// COMPONENTS
+import Navbar from './components/Navbar';
+
+// PAGES 
+import LandingPage from './pages/Landingpage';
+
+// STYLES 
 import './App.css';
-import Navbar from './components/Navbar'
 
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <LandingPage/>
     </div>
   );
 }
